ab3b03ae6567127726a77b600c453df905674b9c
Eleven <xiaoxin@evrazlabs.email>
2020-08-27 17:11:55 +0800

    Initial commit

---
+++ Example/Category_Mine/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  Category_Mine
+//
+//  Created by 33cun on 08/27/2020.
+//  Copyright (c) 2020 33cun. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTViewController : UIViewController
+
+@end
---
+++ Example/Category_Mine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Category_Mine
+//
+//  Created by 33cun on 08/27/2020.
+//  Copyright (c) 2020 33cun. All rights reserved.
+//
+
+@import UIKit;
+#import "MTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
